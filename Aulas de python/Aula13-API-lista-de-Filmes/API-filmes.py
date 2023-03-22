1aa6d8cfab5609089e78a522a27786a4e95d3681
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-03-22 10:11:45 -0400

    continuaçaõ da pratica de API lista de filmes

--- Aulas de python/Aula13-API-lista-de-Filmes/API-filmes.py
+++ Aulas de python/Aula13-API-lista-de-Filmes/API-filmes.py
@@ -1,14 +1,37 @@
 import requests
 import json
 
-req = None
-try:
-    req = requests.get(" http://www.omdbapi.com/?i=tt3896198&apikey=eb931693")
-except:
-    print('Erro na conexão')
-    exit()
+def requisicao(titulo):
+    try:
+        req = requests.get(" http://www.omdbapi.com/?apikey=eb931693&t=" + titulo)
+        dic = json.loads(req.text) # transforma o conteúdo que está em json para forma de dicionario;
+        return dic
+    except:
+        print('Erro na conexão')
+        return None
 
 
-dicionario = json.loads(req.text) # transforma o conteúdo que está em json para forma de dicionario;
+def printar_detalhes(filmes):
+    print('Título: ',filmes['Title'])
+    print('Ano: ',filmes['Year'])
+    print('Diretor: ',filmes['Director'])
+    print('Atores: ', filmes['Actors'])
+    print('Nota: ',filmes['imdbRating'])
+    print('Premios: ',filmes['Awards'])
+    print('Poster:', filmes['Poster'])
+    print('')
 
-print(dicionario)
+
+sair = False
+while not sair:
+    op = input('Escreva o nome de um filme ou SAIR para fechar: ')
+
+    if op == 'SAIR' :
+        SAIR = True
+        print('Saindo...')
+    else:
+        filmes= requisicao(op)
+        if filmes['Response'] == 'False' :
+            print('Filme não encontrado')
+        else:
+            printar_detalhes(filmes)
