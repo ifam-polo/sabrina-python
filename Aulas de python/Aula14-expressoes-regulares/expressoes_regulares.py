a2b9e37ae0eddf94d0ef302897ca7f2b646f5c56
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-03-24 08:35:24 -0400

    finalizando a pratica de expressões regulares

--- Aulas de python/Aula14-expressoes-regulares/expressoes_regulares.py
+++ Aulas de python/Aula14-expressoes-regulares/expressoes_regulares.py
@@ -1,11 +1,15 @@
 import re
 import requests
 
-requisicao = requests.get('https://fpftech.com/')
+requisicao = requests.get('https://fpftech.com/contato/fale-conosco')
 
-padrao = re.findall(r'/[\w\.-]+@[\w-]+\.[\w\.-]+', requisicao.text) # RAW String '. ou \w (os espaços brancos não considera)' para ignorar a caracter seguinte
+padrao = re.findall(r'[\w\.-]+@[\w-]+\.[\w\.-]+', requisicao.text) # RAW String '. ou \w (os espaços brancos não considera)' para ignorar a caracter seguinte
+
+if padrao:
+    print(padrao)
+else:
+    print("Padrão não encontrado")
 
 
-print(padrao)
 
 
